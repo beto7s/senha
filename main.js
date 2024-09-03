5443a4f2d1fda23473abff21627ad6d7e92c580f
beto7s <168612413+beto7s@users.noreply.github.com>
2024-09-03 14:46:44 -0400

    .

--- main.js
+++ main.js
@@ -40,8 +40,40 @@ for (i=0; i < checkbox.length;i++){
 gerasenha(){}
     let alfabeto = ¨;
     if (checkbox[0].checked){
-    alfabeto = alfabeto + letrasMinusculas;
+    alfabeto = alfabeto + letrasMaiusculas;
 }
 if (checkbox{1}.checked){
     alfabeto = alfabeto + letrasMinusculas;
+}
+(checkbox{2}.checked){
+    alfabeto = alfabeto + numeros;
+}
+(checkbox{3}.checked{
+    alfabeto = alfabeto + simbolos;
+})
+console.log(alfabeto);
+let senha = ¨;
+for (let i =0; i <tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+}
+campoSenha.value = senha;
+classificasenha(alfabeto.length);
+
+
+function classificasenha (tamanhoalfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoa1)
+    SVGComponentTransferFunctionElement.log(entropia);
+    forcaSenha.classList.remove(fraca , media , forte);
+    if (tamanhoSenha > 11){
+        forcaSenha.classList.add(forte);
+        else if (tamanhosenha > 5 && tamanhoSenha < 12)
+            else if (tamanhosenha <= 5){
+        forcaSenha.classList.add(fraca);
+    }
+        
+        }
+    }
+
 }
