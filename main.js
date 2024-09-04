ed019e640070e13929aaabd2706005bc9401e1eb
beto7s <168612413+beto7s@users.noreply.github.com>
2024-09-04 09:17:53 -0300

    ç

--- main.js
+++ main.js
@@ -37,22 +37,22 @@ for (i=0; i < checkbox.length;i++){
     checkbox[i].onclick = geraSenha;
 }
 
-gerasenha(){}
-    let alfabeto = ¨;
+ function gerasenha(){
+    let alfabeto = "";
     if (checkbox[0].checked){
     alfabeto = alfabeto + letrasMaiusculas;
 }
 if (checkbox[1].checked){
     alfabeto = alfabeto + letrasMinusculas;
 }
-(checkbox[2].checked){
+if(checkbox[2].checked){
     alfabeto = alfabeto + numeros;
 }
-(checkbox[3].checked{
+if(checkbox[3].checked){
     alfabeto = alfabeto + simbolos;
-})
+}
 console.log(alfabeto);
-let senha = ¨;
+let senha = "";
 for (let i =0; i <tamanhoSenha;i++){
     let numeroAleatorio = Math.random()*alfabeto.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
@@ -60,7 +60,7 @@ for (let i =0; i <tamanhoSenha;i++){
 }
 campoSenha.value = senha;
 classificasenha(alfabeto.length);
-
+ }
 
 function classificasenha (tamanhoalfabeto){
     let entropia = tamanhoSenha * Math.log2(tamanhoa1)
@@ -68,12 +68,14 @@ function classificasenha (tamanhoalfabeto){
     forcaSenha.classList.remove(fraca , media , forte);
     if (tamanhoSenha > 11){
         forcaSenha.classList.add(forte);
-        elseif (tamanhosenha > 5 && tamanhoSenha < 12)
-            elseif (tamanhosenha <= 5){
+    }
+        else if (tamanhosenha > 5 && tamanhoSenha < 12){
+
+        }
+            else if (tamanhosenha <= 5){
         forcaSenha.classList.add(fraca);
     }
         
         }
-    }
+    
 
-}
